63d9d27b73bf368daaad2cc3999ed237e2985b10
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-31 00:33:24 +0900

    :sparkles: Articlesのリレーション設定 author_idをキーとしてUserをauthorに関連付ける

--- lib/realworld/blogs/article.ex
+++ lib/realworld/blogs/article.ex
@@ -2,6 +2,7 @@ defmodule Realworld.Blogs.Article do
   alias Realworld.Blogs.Comment
   alias Realworld.Blogs.ArticleTag
   alias Realworld.Blogs.Tag
+  alias Realworld.Accounts.User
 
   @moduledoc """
   Article Schema
@@ -13,6 +14,7 @@ defmodule Realworld.Blogs.Article do
     field :body, :string
     field :title, :string
 
+    belongs_to :author, User
     has_many :comments, Comment, on_delete: :delete_all
 
     many_to_many :tags, Tag,
@@ -23,13 +25,11 @@ defmodule Realworld.Blogs.Article do
     timestamps()
   end
 
-
-
   @doc false
   def changeset(article, attrs, tags \\ []) do
     article
     |> cast(attrs, [:title, :body])
-    |> validate_required([:title, :body])
+    |> validate_required([:title, :body, :author_id])
     |> put_assoc(:tags, tags)
   end
 end
