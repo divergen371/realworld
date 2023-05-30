f4d711a947764b2a65f268c369e06ef98a504353
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-31 00:29:45 +0900

    :seedling: 記事とユーザーを関連付けるためのスキーマ変更 Articleにauthor_idカラムを追加

---
+++ priv/repo/migrations/20230530152422_add_column_author_id_to_articles.exs
@@ -0,0 +1,14 @@
+defmodule Realworld.Repo.Migrations.AddColumnAuthorIdToArticles do
+  use Ecto.Migration
+
+  def change do
+    alter table("articles") do
+      add(
+        :author_id,
+        references(:users, on_delete: :nothing)
+      )
+    end
+
+    create(index(:articles, [:author_id]))
+  end
+end
