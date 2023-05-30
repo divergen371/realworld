9992872043fa75ce79610c0ba0fc1aa3ee2a35dd
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-30 15:08:39 +0900

    記事とコメントを関連付けた。一対多の関係であるためhas_manyを使用

--- lib/realworld/blogs/article.ex
+++ lib/realworld/blogs/article.ex
@@ -1,4 +1,8 @@
 defmodule Realworld.Blogs.Article do
+  alias Realworld.Blogs.Comment
+  @moduledoc """
+  Article Schema
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
@@ -6,6 +10,7 @@ defmodule Realworld.Blogs.Article do
     field :body, :string
     field :title, :string
 
+    has_many :comments, Comment, on_delete: :delete_all
     timestamps()
   end
 
